651e1c95ade9eee0cdb2518a8557d00f49ec2ca1
Denisse-ds <gojosatoruvss@gmail.com>
2026-05-22 17:43:29 -0600

    Estructura base del proyecto y configuraciones iniciales

--- TareasBlazor/Data/TareasDbContext.cs
+++ TareasBlazor/Data/TareasDbContext.cs
@@ -5,7 +5,7 @@ namespace TareasBlazor.Data
 {
     public class TareasDbContext : DbContext
     {
-        public TareasDbContext(DbContextOptions options)
+        public TareasDbContext(DbContextOptions options) //MODIFICADO: Agregamos el tipo genérico <TareasDbContext> para especificar el contexto
             : base(options)
         {
         }
--- TareasBlazor/Models/Tarea.cs
+++ TareasBlazor/Models/Tarea.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Titulo { get; set; } = "";
         public DateTime FechaVencimiento { get; set; }
-        public bool Completada { get; set; }
+        public bool Completada { get; set; } //MODIFICADO: Cambiamos el tipo de dato a bool para representar si la tarea está completada o no
     }
 }
