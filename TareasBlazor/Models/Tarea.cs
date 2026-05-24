ae8c978d9828cd6abf66690d7ecf0ceecfdcfb29
Denisse-ds <gojosatoruvss@gmail.com>
2026-05-23 21:04:50 -0600

    cambio de prueba

--- TareasBlazor/Models/Tarea.cs
+++ TareasBlazor/Models/Tarea.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Titulo { get; set; } = "";
-        public DateTime FechaVencimiento { get; set; }
+        public DateTime FechaVencimiento { get; set; } //noshe olvidemos de agregar esta propiedad para la fecha de vencimiento
         public bool Completada { get; set; } //MODIFICADO: Cambiamos el tipo de dato a bool para representar si la tarea está completada o no
     }
 }
